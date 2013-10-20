a27c5ca82764cfac7deb8ffd67a4e3d0b69b70a8
chenxiuheng <chenxiuheng@gmail.com>
2013-10-19 20:27:38 -0700

    添加对 metadta tag 的编码

--- streamserver/src/github/chenxh/media/flv/FlvEncoder.java
+++ streamserver/src/github/chenxh/media/flv/FlvEncoder.java
@@ -5,9 +5,11 @@ import github.chenxh.media.flv.script.metadata.FlvMetaData;
 import github.chenxh.media.flv.script.metadata.MetaDataEncoder;
 import github.chenxh.media.flv.struct.TagHeadImpl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class FlvEncoder {
+    
     /**
      * 写出 flv 文件头
      * @param signature
@@ -38,26 +40,57 @@ public class FlvEncoder {
      * @return
      * @throws IOException
      */
+    public byte[] encodeMetaDataTag(FlvMetaData metaData) throws IOException{
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        UnsignedDataOutput output = new UnsignedDataOutput(byteStream);
+        byte[] bytes = new MetaDataEncoder().encode(metaData);
+
+        TagHeadImpl head = new TagHeadImpl();
+        head.init(ITagTrunk.SCRIPT_DATA, bytes.length, 0, 0);
+        
+        encodeTag(head, bytes, output);
+
+        return byteStream.toByteArray();
+    }
+    
+    /**
+     * 写出 scriptdata， 内容为 metadata 信息
+     * 
+     * @param metaData
+     * @param output
+     * @return
+     * @throws IOException
+     */
     public int encodeMetaData(FlvMetaData metaData, UnsignedDataOutput output) throws IOException{
         byte[] bytes = new MetaDataEncoder().encode(metaData);
 
         TagHeadImpl head = new TagHeadImpl();
         head.init(ITagTrunk.SCRIPT_DATA, bytes.length, 0, 0);
         
+
         return encodeTag(head, bytes, output);
     }
     
+    public int encodePreTagSize (long tagSize, UnsignedDataOutput output) throws IOException {
+        output.writeUI32(tagSize);
+        
+        return 4;
+    }
+    
     public int encodeTag(ITagHead head, byte[] bytes, UnsignedDataOutput output) throws IOException{
-        long start = output.writen();
+        output.resetWriten();
+
+        // head
         encodeTagHead(head, output);
         
         // Data
-        output.write(bytes);
+        encodeTagData(head, bytes, output);
         
-        return (int)(output.writen() - start);
+        return (int)(output.writen());
     }
 
-    private void encodeTagHead(ITagHead head, UnsignedDataOutput output) throws IOException {
+    
+    public void encodeTagHead(ITagHead head, UnsignedDataOutput output) throws IOException {
 
         // TagType
         output.writeUI8(head.getTagType());
@@ -71,5 +104,13 @@ public class FlvEncoder {
         
         // streamId
         output.writeUI24(head.getStreamId());
+    }
+    
+    public void encodeTagData(ITagHead head, byte[] data, UnsignedDataOutput output) throws IOException {
+        if (data.length >= head.getDataSize()) {
+            output.write(data, 0, (int)head.getDataSize());
+        } else {
+            output.write(data);
+        }
     }
 }
